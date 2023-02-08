928e9155d42c102f7f275e2cebc8d5c3df13c8c8
Epadrino <epadrinoa@gmail.com>
2023-02-07 20:12:22 -0400

    commit

--- src/components/praticles/paticlesProps.ts
+++
@@ -1,9 +0,0 @@
-import type { Container, ISourceOptions, Engine } from '@tsparticles/engine';
-
-export interface IParticlesProps {
-	id?: string;
-	options?: ISourceOptions;
-	url?: string;
-	particlesInit?: (engine: Engine) => Promise<void>;
-	particlesLoaded?: (container?: Container) => Promise<void>;
-}
