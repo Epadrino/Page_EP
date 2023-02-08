7167cc623e1c4285aaac8d2d4635b4b378a8ce5e
Epadrino <epadrinoa@gmail.com>
2023-02-07 20:01:44 -0400

    commit

---
+++ src/components/praticles/index.ts
@@ -0,0 +1,13 @@
+import Particles from './Particles';
+
+export type {
+	IParticlesProps,
+	IParticlesProps as ParticlesProps,
+} from './paticlesProps';
+export type {
+	IParticlesState,
+	IParticlesState as ParticlesState,
+} from './particlesState';
+
+export default Particles;
+export { Particles };
---
+++ src/components/praticles/paticlesProps.ts
@@ -0,0 +1,9 @@
+import type { Container, ISourceOptions, Engine } from '@tsparticles/engine';
+
+export interface IParticlesProps {
+	id?: string;
+	options?: ISourceOptions;
+	url?: string;
+	particlesInit?: (engine: Engine) => Promise<void>;
+	particlesLoaded?: (container?: Container) => Promise<void>;
+}
